2f1a347f538f5155d226cf373d6760f7c2051ed8
Rosimary <Privado_mary@hotmail.com>
2023-08-04 20:48:52 -0300

    p3,p4

--- perfil/models.py
+++ perfil/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    categoria = models.CharField(max_length=50,null=False,blank=False, verbose_name='Categoria')
+    essencial = models.BooleanField(default=False)
+    valor_planejamento = models.FloatField(default=0, verbose_name='Valor do planejamento')
+    
+    def __str__(self):
+        return self.categoria
+
+class Conta(models.Model):
+    banco_choices = (
+        ('Nu', 'Nubank'),
+        ('BB','Banco do Brasil'),
+        ('IT','Itau'),
+        ('CX','Caixa'),
+        ('BD','Bradesco'),
+    )
+    
+    tipo_choices = (
+        ('pf','Pessoa Fisica'),
+        ('pj', 'Pessoa Juridica')
+    )
+    apelido = models.CharField(max_length=50)
+    banco =models.CharField(max_length=2, choices=banco_choices)
+    tipo = models.CharField(max_length=2, choices=tipo_choices)
+    valor = models.FloatField()
+    icone =  models.ImageField(upload_to='icone')
+    
+    def __str__(self):
+        return self.apelido
+    
--- perfil/views.py
+++ perfil/views.py
@@ -4,3 +4,14 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request,'home.html')
+
+def gerenciar(request):
+    return render(request, 'gerenciar.html')
+
+def cadastrar_banco(request):
+    apelido = request.POST.get('apelido')
+    banco = request.POST.get('banco')
+    tipo = request.POST.get('tipo')
+    valor = request.POST.get('valor')
+    icone = request.POST.get('icone')
+    
