deb1941cbd8051a2c118c348739fe50479ecb72d
Rosimary <Privado_mary@hotmail.com>
2023-08-08 20:07:20 -0300

    p6

--- perfil/views.py
+++ perfil/views.py
@@ -9,7 +9,13 @@ def home(request):
     return render(request,'home.html')
 
 def gerenciar(request):
-    return render(request, 'gerenciar.html')
+    contas = Conta.objects.all()
+    
+    total_contas =  0
+    for conta in contas:
+        total_contas += conta.valor
+        
+    return render(request, 'gerenciar.html', {'contas': contas, 'total_contas': total_contas})
 
 def cadastrar_banco(request):
     apelido = request.POST.get('apelido')
